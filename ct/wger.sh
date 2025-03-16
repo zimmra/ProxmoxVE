b1b08cd819850deb1ec8efa36e0d8b21095377bb
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-03-16 18:33:23 +0100

    fix build.func path (#3121)

--- ct/wger.sh
+++ ct/wger.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/tremor021/ProxmoxVE/refs/heads/wger/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: Slaviša Arežina (tremor021)
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
