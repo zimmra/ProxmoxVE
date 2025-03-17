bff8879189ddba7515fddaafe938563b17103e2b
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-03-17 13:13:49 +0100

    InfluxDB: Add Ports as Info / Script-End (#3141)
    * InfluxDB: Add Ports as Info / Script-End
    
    * Update influxdb.sh

--- ct/influxdb.sh
+++ ct/influxdb.sh
@@ -41,4 +41,4 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8086${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP} and Port 8888 for v1 or Port 8086 (v2)${CL}"
