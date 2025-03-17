2f45ae45bf098bbc4dd66c443d2c1bdf05675ee2
Daniel Q <daniel@familie-quolke.de>
2025-03-17 15:00:21 +0100

    Fixed wrong PHP values to match default part-db size (100M) (#3143)
    * Fixed wrong PHP values to match default part-db size (100M)
    
    The default Uploadsize is 100M but PHP is limited to 2M (Upload) and 8M (Post) by default, this commit fixed that.
    
    * Getting current used php-version

--- install/part-db-install.sh
+++ install/part-db-install.sh
@@ -29,6 +29,12 @@ $STD apt-get install -y \
   postgresql
 msg_ok "Installed Dependencies"
 
+msg_info "Setting up PHP"
+PHPVER=$(php -r 'echo PHP_MAJOR_VERSION . "." . PHP_MINOR_VERSION . "\n";')
+sed -i "s@post_max_size = 8M@post_max_size = 100M@g" /etc/php/${PHPVER}/apache2/php.ini 
+sed -i "s@upload_max_filesize = 2M@upload_max_filesize = 100M@g" /etc/php/${PHPVER}/apache2/php.ini
+msg_ok "Setting up PHP"
+
 msg_info "Setting up PostgreSQL"
 DB_NAME=partdb
 DB_USER=partdb
