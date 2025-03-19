1dc1165894119c664a58f55e375c52f4a00d7323
Bas van den Berg <74251551+bvdberg01@users.noreply.github.com>
2025-03-19 10:21:10 +0100

    License VED to VE (#3258)

--- ct/paperless-gpt.sh
+++ ct/paperless-gpt.sh
@@ -2,7 +2,7 @@
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: MickLesk (CanbiZ)
-# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/icereed/paperless-gpt
 
 APP="Paperless-GPT"
--- install/paperless-gpt-install.sh
+++ install/paperless-gpt-install.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: MickLesk (CanbiZ)
-# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://github.com/icereed/paperless-gpt
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
