7d0f60c8d14889f715b2ccb6f2aa64870ae64b1e
Bas van den Berg <74251551+bvdberg01@users.noreply.github.com>
2025-03-24 15:27:18 +0100

    Small issues fixed (#3386)

--- ct/wazuh.sh
+++ ct/wazuh.sh
@@ -23,7 +23,7 @@ function update_script() {
    header_info
    check_container_storage
    check_container_resources
-   if [[ ! -d /var ]]; then
+   if [[ ! -f /lib/systemd/system/wazuh-manager.service ]]; then
       msg_error "No ${APP} Installation Found!"
       exit
    fi
--- install/wazuh-install.sh
+++ install/wazuh-install.sh
@@ -13,22 +13,13 @@ setting_up_container
 network_check
 update_os
 
-msg_info "Installing Dependencies"
-$STD apt-get install -y \
-  sudo \
-  mc \
-  curl
-msg_ok "Installed Dependencies"
-
-# Fetching the latest Wazuh version
 msg_info "Fetching Latest Wazuh Version"
 RELEASE=$(curl -s https://api.github.com/repos/wazuh/wazuh/releases/latest | grep '"tag_name"' | awk -F '"' '{print substr($4, 2, length($2)-4)}')
 msg_ok "Latest Wazuh Version: $RELEASE"
 
 msg_info "Setup Wazuh"
-curl -fsSL https://packages.wazuh.com/$RELEASE/wazuh-install.sh
+curl -fsSL https://packages.wazuh.com/$RELEASE/wazuh-install.sh -o wazuh-install.sh
 chmod +x wazuh-install.sh
-
 if [ "$STD" = "silent" ]; then
   bash wazuh-install.sh -a >>~/wazuh-install.output
 else
