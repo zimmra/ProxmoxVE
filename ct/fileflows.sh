32741150417eb7274703e41ddcf8b9d9845145f0
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-03-27 10:20:57 +0100

    harmonize licence to VE (not VED)

--- ct/fileflows.sh
+++ ct/fileflows.sh
@@ -2,7 +2,7 @@
 source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: kkroboth
-# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://fileflows.com/
 
 APP="FileFlows"
--- install/fileflows-install.sh
+++ install/fileflows-install.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: kkroboth
-# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
 # Source: https://fileflows.com/
 
 # Import Functions und Setup
