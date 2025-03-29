8a5367dae385ba1bdd81bba7ee96d9f680fa8a52
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-03-29 12:21:58 +0100

    Fix temp file (#3476)

--- install/ersatztv-install.sh
+++ install/ersatztv-install.sh
@@ -35,6 +35,7 @@ fi
 msg_ok "Set Up Hardware Acceleration"
 
 msg_info "Installing ErsatzTV"
+temp_file=$(mktemp)
 cd /opt
 RELEASE=$(curl -s https://api.github.com/repos/ErsatzTV/ErsatzTV/releases | grep -oP '"tag_name": "\K[^"]+' | head -n 1)
 wget -qO- "https://github.com/ErsatzTV/ErsatzTV/releases/download/${RELEASE}/ErsatzTV-${RELEASE}-linux-x64.tar.gz" -O "$temp_file"
