8e633636a6a15cd7e06fda12772710e5301d5c71
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-04-01 22:14:13 +0200

    Fix curl command (#3584)

--- install/flaresolverr-install.sh
+++ install/flaresolverr-install.sh
@@ -29,10 +29,10 @@ msg_ok "Installed Chrome"
 
 msg_info "Installing FlareSolverr"
 RELEASE=$(curl -fsSL https://github.com/FlareSolverr/FlareSolverr/releases/latest | grep "title>Release" | cut -d " " -f 4)
-$STD curl -fsSL "https://github.com/FlareSolverr/FlareSolverr/releases/download/$RELEASE/flaresolverr_linux_x64.tar.gz" -O "flaresolverr_linux_x64.tar.gz"
+$STD curl -fsSL "https://github.com/FlareSolverr/FlareSolverr/releases/download/$RELEASE/flaresolverr_linux_x64.tar.gz" -o "flaresolverr_linux_x64.tar.gz"
 $STD tar -xzf flaresolverr_linux_x64.tar.gz -C /opt
 $STD rm flaresolverr_linux_x64.tar.gz
-echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
+echo "${RELEASE}" >/opt/"${APPLICATION}"_version.txt
 msg_ok "Installed FlareSolverr"
 
 msg_info "Creating Service"
