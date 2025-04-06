c08ebc48e8889d8a0554ab2420d19d0743166e5a
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-04-06 15:48:27 +0200

    Pelican-Panel: Fix PHP 8.4 Repository (#3700)

--- install/pelican-panel-install.sh
+++ install/pelican-panel-install.sh
@@ -23,7 +23,7 @@ $STD apt-get install -y \
 msg_ok "Installed Dependencies"
 
 msg_info "Adding PHP8.4 Repository"
-$STD curl -sSLo "https://packages.sury.org/debsuryorg-archive-keyring.deb" -o /tmp/debsuryorg-archive-keyring.deb
+$STD curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
 $STD dpkg -i /tmp/debsuryorg-archive-keyring.deb
 $STD sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
 $STD apt-get update
