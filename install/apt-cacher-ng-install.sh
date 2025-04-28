246c6e950d09ba58ec3103c0764aafc2371f1875
Patrick Cudahy <pgcudahy@gmail.com>
2025-04-28 14:50:47 +0200

    Make apt-cacher-ng a client of its own server (#4092)
    Per https://askubuntu.com/questions/1503184/apt-cacher-ng-not-caching-packages-installed-on-the-server

--- install/apt-cacher-ng-install.sh
+++ install/apt-cacher-ng-install.sh
@@ -16,6 +16,9 @@ update_os
 msg_info "Installing Apt-Cacher NG"
 DEBIAN_FRONTEND=noninteractive $STD apt-get -o Dpkg::Options::="--force-confold" install -y apt-cacher-ng
 sed -i 's/# PassThroughPattern: .* # this would allow CONNECT to everything/PassThroughPattern: .*/' /etc/apt-cacher-ng/acng.conf
+cat << EOF >/etc/apt/apt.conf.d/00aptproxy.conf
+Acquire::http::Proxy "http://localhost:3142";
+EOF
 systemctl enable -q --now apt-cacher-ng
 msg_ok "Installed Apt-Cacher NG"
 
