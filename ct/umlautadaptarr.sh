c3b69f40fc786e8adbf5e7b2365d6db04ddcf3e5
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-04-29 08:01:41 +0200

    ved -> ve

--- ct/umlautadaptarr.sh
+++ ct/umlautadaptarr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/refs/heads/main/misc/build.func)
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/refs/heads/main/misc/build.func)
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: elvito
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
