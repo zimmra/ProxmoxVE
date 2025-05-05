e5766da56daf072efcbdae4fa9ad43257627b2a1
CanbiZ <canbiz.dev@gmail.com>
2025-05-05 22:33:49 +0200

    Update docker-vm.sh

--- vm/docker-vm.sh
+++ vm/docker-vm.sh
@@ -510,7 +510,7 @@ EOF
 )
 qm set "$VMID" -description "$DESCRIPTION" >/dev/null
 
-f [ -n "$DISK_SIZE" ]; then
+if [ -n "$DISK_SIZE" ]; then
   msg_info "Resizing disk to $DISK_SIZE GB"
   qm resize $VMID scsi0 ${DISK_SIZE} >/dev/null
 else
