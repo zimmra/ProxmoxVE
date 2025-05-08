fabbe8fe59d610d8295c18d62aa6bf734975ce08
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-05-08 10:50:45 +0200

    Update streamlink-webui.sh (#4314)

--- ct/streamlink-webui.sh
+++ ct/streamlink-webui.sh
@@ -24,7 +24,7 @@ function update_script() {
   check_container_storage
   check_container_resources
 
-  if [[ ! -f /opt/streamlink-webui ]]; then
+  if [[ ! -d /opt/streamlink-webui ]]; then
     msg_error "No ${APP} Installation Found!"
     exit
   fi
