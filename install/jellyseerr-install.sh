1e59c934ec0ce5a97bcd4f4cd5c31be8ee24bacc
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-10 08:42:45 +0200

    Jellyseerr: better handling of node and pnpm (#4365)
    * Jellyseerr: better handling of node and pnpm
    
    * Update jellyseerr.sh

--- install/jellyseerr-install.sh
+++ install/jellyseerr-install.sh
@@ -15,30 +15,18 @@ update_os
 
 msg_info "Installing Dependencies"
 $STD apt-get install -y \
-    gnupg \
     git \
     build-essential
 msg_ok "Installed Dependencies"
 
-msg_info "Setting up Node.js Repository"
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_22.x nodistro main" >/etc/apt/sources.list.d/nodesource.list
-msg_ok "Set up Node.js Repository"
 
-msg_info "Installing Node.js"
-$STD apt-get update
-$STD apt-get install -y nodejs
-msg_ok "Installed Node.js"
 
 git clone -q https://github.com/Fallenbagel/jellyseerr.git /opt/jellyseerr
 cd /opt/jellyseerr
 $STD git checkout main
 
 pnpm_desired=$(grep -Po '"pnpm":\s*"\K[^"]+' /opt/jellyseerr/package.json)
-msg_info "Installing pnpm version $pnpm_desired..."
-$STD npm install -g pnpm@$pnpm_desired
-msg_ok "Installed pnpm"
+NODE_VERSION="22" NODE_MODULE="pnpm@$pnpm_desired" install_node_and_modules
 
 msg_info "Installing Jellyseerr (Patience)"
 export CYPRESS_INSTALL_BINARY=0
