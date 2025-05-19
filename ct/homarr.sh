2a10eca156aa1d21da590c1c64969ea2b4f79734
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-05-19 13:42:39 +0200

    increase ressources of Homarr (3 vCPU / 6GB RAM) (#4583)

--- ct/homarr.sh
+++ ct/homarr.sh
@@ -7,8 +7,8 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 
 APP="homarr"
 var_tags="${var_tags:-arr;dashboard}"
-var_cpu="${var_cpu:-2}"
-var_ram="${var_ram:-4096}"
+var_cpu="${var_cpu:-3}"
+var_ram="${var_ram:-6144}"
 var_disk="${var_disk:-8}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-12}"
