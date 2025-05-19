2c2e602eb184017d072969a23ab914c412a802a7
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-05-19 18:52:54 +0200

    RustDesk Server: Add WebUI (#4590)
    * Add WebUI
    
    * Update
    
    * Update port
    
    * Update

--- ct/rustdeskserver.sh
+++ ct/rustdeskserver.sh
@@ -20,42 +20,54 @@ color
 catch_errors
 
 function update_script() {
-    header_info
-    check_container_storage
-    check_container_resources
+  header_info
+  check_container_storage
+  check_container_resources
 
-    if [[ ! -x /usr/bin/hbbr ]]; then
-        msg_error "No ${APP} Installation Found!"
-        exit
-    fi
-    RELEASE=$(curl -fsSL https://api.github.com/repos/rustdesk/rustdesk-server/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-    if [[ "${RELEASE}" != "$(cat /opt/rustdesk_version.txt)" ]] || [[ ! -f /opt/rustdesk_version.txt ]]; then
-        msg_info "Stopping $APP"
-        systemctl stop rustdesk-hbbr
-        systemctl stop rustdesk-hbbs
-        msg_ok "Stopped $APP"
-
-        msg_info "Updating $APP to v${RELEASE}"
-        TEMPDIR=$(mktemp -d)
-        curl -fsSL "https://github.com/rustdesk/rustdesk-server/releases/download/${RELEASE}/rustdesk-server-hbbr_${RELEASE}_amd64.deb" \
-            -o "${TEMPDIR}/rustdesk-server-hbbr_${RELEASE}_amd64.deb"
-        curl -fsSL "https://github.com/rustdesk/rustdesk-server/releases/download/${RELEASE}/rustdesk-server-hbbs_${RELEASE}_amd64.deb" \
-            -o "${TEMPDIR}/rustdesk-server-hbbs_${RELEASE}_amd64.deb"
-        curl -fsSL "https://github.com/rustdesk/rustdesk-server/releases/download/${RELEASE}/rustdesk-server-utils_${RELEASE}_amd64.deb" \
-            -o "${TEMPDIR}/rustdesk-server-utils_${RELEASE}_amd64.deb"
-        $STD dpkg -i $TEMPDIR/*.deb
-        msg_ok "Updated $APP to v${RELEASE}"
-
-        msg_info "Cleaning Up"
-        rm -rf $TEMPDIR
-        msg_ok "Cleanup Completed"
-
-        echo "${RELEASE}" >/opt/rustdesk_version.txt
-        msg_ok "Update Successful"
-    else
-        msg_ok "No update required. ${APP} is already at v${RELEASE}"
-    fi
+  if [[ ! -x /usr/bin/hbbr ]]; then
+    msg_error "No ${APP} Installation Found!"
     exit
+  fi
+  if [[ ! -f /opt/rustdeskapi_version.txt ]]; then
+    touch /opt/rustdeskapi_version.txt
+    exit
+  fi
+  RELEASE=$(curl -fsSL https://api.github.com/repos/rustdesk/rustdesk-server/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+  APIRELEASE=$(curl -fsSL https://api.github.com/repos/lejianwen/rustdesk-api/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+  if [[ "${RELEASE}" != "$(cat /opt/rustdesk_version.txt)" ]] || [[ "${APIRELEASE}" != "$(cat /opt/rustdesk-api.txt)" ]] || [[ ! -f /opt/rustdesk_version.txt ]] || [[ ! -f /opt/rustdesk-api.txt ]]; then
+    msg_info "Stopping $APP"
+    systemctl stop rustdesk-hbbr
+    systemctl stop rustdesk-hbbs
+    systemctl stop rustdesk-api
+    msg_ok "Stopped $APP"
+
+    msg_info "Updating $APP to v${RELEASE}"
+    TEMPDIR=$(mktemp -d)
+    curl -fsSL "https://github.com/rustdesk/rustdesk-server/releases/download/${RELEASE}/rustdesk-server-hbbr_${RELEASE}_amd64.deb" \
+      -o "${TEMPDIR}/rustdesk-server-hbbr_${RELEASE}_amd64.deb"
+    curl -fsSL "https://github.com/rustdesk/rustdesk-server/releases/download/${RELEASE}/rustdesk-server-hbbs_${RELEASE}_amd64.deb" \
+      -o "${TEMPDIR}/rustdesk-server-hbbs_${RELEASE}_amd64.deb"
+    curl -fsSL "https://github.com/rustdesk/rustdesk-server/releases/download/${RELEASE}/rustdesk-server-utils_${RELEASE}_amd64.deb" \
+      -o "${TEMPDIR}/rustdesk-server-utils_${RELEASE}_amd64.deb"
+    curl -fsSL "https://github.com/lejianwen/rustdesk-api/releases/download/v${APIRELEASE}/rustdesk-api-server_${APIRELEASE}_amd64.deb" \
+      -o "${TEMPDIR}/rustdesk-api-server_${APIRELEASE}_amd64.deb"
+    $STD dpkg -i $TEMPDIR/*.deb
+    echo "${RELEASE}" >/opt/rustdesk_version.txt
+    echo "${APIRELEASE}" >/opt/rustdeskapi_version.txt
+    msg_ok "Updated $APP to v${RELEASE}"
+
+    msg_info "Cleaning Up"
+    rm -rf $TEMPDIR
+    msg_ok "Cleanup Completed"
+
+    msg_info "Starting services"
+    systemctl start -q rustdesk-* --all
+    msg_ok "Services started"
+    msg_ok "Update Successful"
+  else
+    msg_ok "No update required. ${APP} is already at v${RELEASE}"
+  fi
+  exit
 }
 
 start
@@ -65,4 +77,4 @@ description
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following URL:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}${IP}${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}${IP}:21114${CL}"
--- install/rustdeskserver-install.sh
+++ install/rustdeskserver-install.sh
@@ -16,15 +16,27 @@ update_os
 msg_info "Setup RustDesk"
 
 RELEASE=$(curl -fsSL https://api.github.com/repos/rustdesk/rustdesk-server/releases/latest | grep "tag_name" | awk -F '"' '{print $4}')
+APIRELEASE=$(curl -fsSL https://api.github.com/repos/lejianwen/rustdesk-api/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
 TEMPDIR=$(mktemp -d)
 
 curl -fsSL "https://github.com/rustdesk/rustdesk-server/releases/download/${RELEASE}/rustdesk-server-hbbr_${RELEASE}_amd64.deb" \
-    -o "${TEMPDIR}/rustdesk-server-hbbr_${RELEASE}_amd64.deb"
+  -o "${TEMPDIR}/rustdesk-server-hbbr_${RELEASE}_amd64.deb"
 curl -fsSL "https://github.com/rustdesk/rustdesk-server/releases/download/${RELEASE}/rustdesk-server-hbbs_${RELEASE}_amd64.deb" \
-    -o "${TEMPDIR}/rustdesk-server-hbbs_${RELEASE}_amd64.deb"
+  -o "${TEMPDIR}/rustdesk-server-hbbs_${RELEASE}_amd64.deb"
 curl -fsSL "https://github.com/rustdesk/rustdesk-server/releases/download/${RELEASE}/rustdesk-server-utils_${RELEASE}_amd64.deb" \
-    -o "${TEMPDIR}/rustdesk-server-utils_${RELEASE}_amd64.deb"
+  -o "${TEMPDIR}/rustdesk-server-utils_${RELEASE}_amd64.deb"
+curl -fsSL "https://github.com/lejianwen/rustdesk-api/releases/download/v${APIRELEASE}/rustdesk-api-server_${APIRELEASE}_amd64.deb" \
+  -o "${TEMPDIR}/rustdesk-api-server_${APIRELEASE}_amd64.deb"
 $STD dpkg -i "${TEMPDIR}"/*.deb
+ADMINPASS=$(openssl rand -base64 18 | tr -dc 'a-zA-Z0-9' | head -c13)
+cd /var/lib/rustdesk-api
+$STD rustdesk-api reset-admin-pwd $ADMINPASS
+{
+  echo "RustDesk WebUI"
+  echo ""
+  echo "Username: admin"
+  echo "Password: $ADMINPASS"
+} >>~/rustdesk.creds
 echo "${RELEASE}" >/opt/rustdesk_version.txt
 msg_ok "Setup RustDesk"
 
