72c24d5147991953574b04ac27b47750a9d89638
Théophile Diot <57175294+TheophileDiot@users.noreply.github.com>
2025-05-27 16:09:55 +0200

    Increase default RAM allocation for BunkerWeb to 8192MB in script and JSON configuration (#4762)

--- ct/bunkerweb.sh
+++ ct/bunkerweb.sh
@@ -8,7 +8,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 APP="BunkerWeb"
 var_tags="${var_tags:-webserver}"
 var_cpu="${var_cpu:-2}"
-var_ram="${var_ram:-4096}"
+var_ram="${var_ram:-8192}"
 var_disk="${var_disk:-4}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-12}"
