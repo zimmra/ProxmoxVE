d9c4ff9867534bacdde16612380d6a7b177f21c4
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-05-27 21:13:36 +0200

    Update alpine-vaultwarden.sh (#4772)

--- ct/alpine-vaultwarden.sh
+++ ct/alpine-vaultwarden.sh
@@ -66,4 +66,4 @@ description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${APP} should be reachable by going to the following URL.
-         ${BL}http://${IP}:8000${CL} \n"
+         ${BL}https://${IP}:8000${CL} \n"
