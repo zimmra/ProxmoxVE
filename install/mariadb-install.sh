338d351c29fe0d9e133f4e154275741475b2e604
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-06-05 09:59:32 +0200

    Update phpMyAdmin for MariaDB and MySQL LXC's (#4983)
    * Update mariadb-install.sh
    
    * Update

--- install/mariadb-install.sh
+++ install/mariadb-install.sh
@@ -33,9 +33,9 @@ if [[ ${prompt,,} =~ ^(y|yes)$ ]]; then
     php-json \
     php-curl
 
-  curl -fsSL "https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.tar.gz" -o "phpMyAdmin-5.2.1-all-languages.tar.gz"
+  curl -fsSL "https://files.phpmyadmin.net/phpMyAdmin/5.2.2/phpMyAdmin-5.2.2-all-languages.tar.gz" -o "phpMyAdmin-5.2.2-all-languages.tar.gz"
   mkdir -p /var/www/html/phpMyAdmin
-  tar xf phpMyAdmin-5.2.1-all-languages.tar.gz --strip-components=1 -C /var/www/html/phpMyAdmin
+  tar xf phpMyAdmin-5.2.2-all-languages.tar.gz --strip-components=1 -C /var/www/html/phpMyAdmin
   cp /var/www/html/phpMyAdmin/config.sample.inc.php /var/www/html/phpMyAdmin/config.inc.php
   SECRET=$(openssl rand -base64 24)
   sed -i "s#\$cfg\['blowfish_secret'\] = '';#\$cfg['blowfish_secret'] = '${SECRET}';#" /var/www/html/phpMyAdmin/config.inc.php
