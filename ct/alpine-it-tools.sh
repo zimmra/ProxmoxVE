33e49b75567a695b002d691f0b47287bcb179f3d
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-06-07 20:19:56 +0200

    fix alpine-it-tools update (#5039)

--- ct/alpine-it-tools.sh
+++ ct/alpine-it-tools.sh
@@ -21,8 +21,6 @@ catch_errors
 
 function update_script() {
   header_info
-  check_container_storage
-  check_container_resources
 
   if [ ! -d /usr/share/nginx/html ]; then
     msg_error "No ${APP} Installation Found!"
