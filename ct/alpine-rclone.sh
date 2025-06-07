70d6815d91910a55db8a432a6887bdaa784b56f4
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-06-07 23:32:38 +0200

    Remove storage checks (#5047)

--- ct/alpine-rclone.sh
+++ ct/alpine-rclone.sh
@@ -22,9 +22,6 @@ catch_errors
 
 function update_script() {
   header_info
-  check_container_storage
-  check_container_resources
-
   if [ ! -d /opt/rclone ]; then
     msg_error "No ${APP} Installation Found!"
     exit 1
