c2c5159a2d79cf1b90aafb4c3174e84c44a81db9
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-06-12 21:31:32 +0200

    Kasm: Increase Ressources & Hint for Fuse / Swap (#5112)
    * Kasm: Increase Ressources & Hint for Fuse / Swap
    
    * extend info

--- ct/kasm.sh
+++ ct/kasm.sh
@@ -8,7 +8,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 APP="Kasm"
 var_tags="${var_tags:-os}"
 var_cpu="${var_cpu:-2}"
-var_ram="${var_ram:-4192}"
+var_ram="${var_ram:-8192}"
 var_disk="${var_disk:-30}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-12}"
