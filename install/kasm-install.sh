9f26e25126160df5e0b056c3938eafa958143a33
Omar Minaya <omiinaya@gmail.com>
2025-06-16 04:16:51 -0400

    Kasm: Storing Creds Fix (#5162)
    * this should do it.
    
    * Update kasm-install.sh
    
    ---------
    
    Co-authored-by: Michel Roegl-Brunner <73236783+michelroegl-brunner@users.noreply.github.com>

--- install/kasm-install.sh
+++ install/kasm-install.sh
@@ -16,17 +16,25 @@ update_os
 msg_info "Installing Kasm Workspaces"
 KASM_VERSION=$(curl -fsSL 'https://www.kasmweb.com/downloads' | grep -o 'https://kasm-static-content.s3.amazonaws.com/kasm_release_[^"]*\.tar\.gz' | head -n 1 | sed -E 's/.*release_(.*)\.tar\.gz/\1/')
 curl -fsSL -o "/opt/kasm_release_${KASM_VERSION}.tar.gz" "https://kasm-static-content.s3.amazonaws.com/kasm_release_${KASM_VERSION}.tar.gz"
-tar -xf "/opt/kasm_release_${KASM_VERSION}.tar.gz" -C /opt
-printf 'y\ny\ny\n4\n' | bash /opt/kasm_release/install.sh
-touch ~/kasm-install.output
-sed -n '/Kasm UI Login Credentials/,$p' ~/kasm-install.output >~/kasm.creds
+
+cd /opt
+tar -xf "kasm_release_${KASM_VERSION}.tar.gz"
+chmod +x /opt/kasm_release/install.sh
+printf 'y\ny\ny\n4\n' | bash /opt/kasm_release/install.sh > ~/kasm-install.output 2>&1
+cat ~/kasm-install.output | grep -A 20 -i "credentials\|login\|password\|admin" | sed '1i Kasm-Workspaces-Credentials' >~/kasm.creds
+
 msg_ok "Installed Kasm Workspaces"
 
 motd_ssh
 customize
 
+msg_info "Displaying Kasm Credentials"
+cat ~/kasm.creds
+msg_ok "Kasm Credentials displayed"
+
 msg_info "Cleaning up"
-$STD rm -f "/opt/kasm_release_${KASM_VERSION}.tar.gz"
+rm -f /opt/kasm_release_${KASM_VERSION}.tar.gz
+rm -f ~/kasm-install.output
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
