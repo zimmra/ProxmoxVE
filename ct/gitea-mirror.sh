4a1ae5144629f2d5b1f925661e42bd76dc0d7508
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2025-06-17 23:06:06 +0200

    gitea-mirror: increase build ressources  (#5235)

--- ct/gitea-mirror.sh
+++ ct/gitea-mirror.sh
@@ -7,9 +7,9 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 
 APP="gitea-mirror"
 var_tags="${var_tags:-mirror;gitea}"
-var_cpu="${var_cpu:-1}"
-var_ram="${var_ram:-1024}"
-var_disk="${var_disk:-5}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-2048}"
+var_disk="${var_disk:-6}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-12}"
 var_unprivileged="${var_unprivileged:-1}"
