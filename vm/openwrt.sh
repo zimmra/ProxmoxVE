82179752ee42cbb18d359435d7f4eb9bec4670eb
Götz Görisch <47734341+GoetzGoerisch@users.noreply.github.com>
2025-06-22 21:40:54 +0200

    fix: casing and naming error after #5254 (#5380)
    * fix: fix casing of OpenWrt
    
    Signed-off-by: Goetz Goerisch <ggoerisch@gmail.com>
    
    * fix: correct name of Homeassistant VM for PiMox
    
    * follow to #5254
    
    Signed-off-by: Goetz Goerisch <ggoerisch@gmail.com>
    
    ---------
    
    Signed-off-by: Goetz Goerisch <ggoerisch@gmail.com>
    Co-authored-by: Goetz Goerisch <ggoerisch@gmail.com>

--- vm/openwrt.sh
+++ vm/openwrt.sh
@@ -516,7 +516,7 @@ DESCRIPTION=$(
     <img src='https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/images/logo-81x112.png' alt='Logo' style='width:81px;height:112px;'/>
   </a>
 
-  <h2 style='font-size: 24px; margin: 20px 0;'>OpenWRT VM</h2>
+  <h2 style='font-size: 24px; margin: 20px 0;'>OpenWrt VM</h2>
 
   <p style='margin: 16px 0;'>
     <a href='https://ko-fi.com/community_scripts' target='_blank' rel='noopener noreferrer'>
