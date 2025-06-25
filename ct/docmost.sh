5ad9323944c53db8fb6d64d7ede0f40a36fa9897
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-06-25 14:25:17 +0200

    Docmost: Increase resources (#5458)
    * Update docmost.sh
    
    * Update docmost.json

--- ct/docmost.sh
+++ ct/docmost.sh
@@ -8,8 +8,8 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 APP="Docmost"
 var_tags="${var_tags:-documents}"
 var_cpu="${var_cpu:-3}"
-var_ram="${var_ram:-3072}"
-var_disk="${var_disk:-7}"
+var_ram="${var_ram:-4096}"
+var_disk="${var_disk:-8}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-12}"
 
