3f3278b02226a294666d7bb5e3c03ce0d8136c23
Mészöly Marcell <62993044+meszolym@users.noreply.github.com>
2025-06-30 22:36:11 +0200

    Fixing Cloudflare DDNS - lack of resources (#5600)

--- ct/cloudflare-ddns.sh
+++ ct/cloudflare-ddns.sh
@@ -7,8 +7,8 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 
 APP="Cloudflare-DDNS"
 var_tags="${var_tags:-network}"
-var_cpu="${var_cpu:-1}"
-var_ram="${var_ram:-512}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-1024}"
 var_disk="${var_disk:-3}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-12}"
