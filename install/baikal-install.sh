e492a9b5d92858c46ccb0fd3ef9f0c20319c1afa
Slaviša Arežina <58952836+tremor021@users.noreply.github.com>
2025-07-06 10:43:54 +0200

    Refactor (#5736)

--- install/baikal-install.sh
+++ install/baikal-install.sh
@@ -14,13 +14,12 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apt-get install -y \
-  apache2 \
-  libapache2-mod-php \
-  php-{pgsql,dom}
+$STD apt-get install -y apache2
 msg_ok "Installed Dependencies"
 
 PG_VERSION="16" setup_postgresql
+PHP_APACHE="YES" PHP_MODULE="pgsql, dom" PHP_VERSION="8.2" setup_php
+fetch_and_deploy_gh_release "baikal" "sabre-io/Baikal"
 
 msg_info "Setting up PostgreSQL Database"
 DB_NAME=baikal
@@ -36,11 +35,7 @@ $STD sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER TEMP
 } >>~/baikal.creds
 msg_ok "Set up PostgreSQL Database"
 
-msg_info "Installing Baikal"
-RELEASE=$(curl -fsSL https://api.github.com/repos/sabre-io/Baikal/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-cd /opt
-curl -fsSL "https://github.com/sabre-io/baikal/releases/download/${RELEASE}/baikal-${RELEASE}.zip" -o "baikal-${RELEASE}.zip"
-$STD unzip "baikal-${RELEASE}.zip"
+msg_info "Configuring Baikal"
 cat <<EOF >/opt/baikal/config/baikal.yaml
 database:
     backend: pgsql
@@ -51,7 +46,6 @@ database:
 EOF
 chown -R www-data:www-data /opt/baikal/
 chmod -R 755 /opt/baikal/
-echo "${RELEASE}" >/opt/${APPLICATION}_version.txt
 msg_ok "Installed Baikal"
 
 msg_info "Creating Service"
@@ -90,7 +84,6 @@ motd_ssh
 customize
 
 msg_info "Cleaning up"
-rm -rf "/opt/baikal-${RELEASE}.zip"
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
 msg_ok "Cleaned"
