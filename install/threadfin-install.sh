c116e773b36cbfb1ff3f10ca2cc166b7c15229d5
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2025-07-14 08:54:56 +0200

    qf: threadfin

--- install/threadfin-install.sh
+++ install/threadfin-install.sh
@@ -20,7 +20,7 @@ $STD apt-get install -y \
 msg_ok "Installed Dependencies"
 
 fetch_and_deploy_gh_release "threadfin" "threadfin/threadfin" "singlefile" "latest" "/opt/threadfin" "Threadfin_linux_amd64"
-rm -rf /root/.threadfin
+mv /root/.threadfin /root/.threadfin_version
 mkdir -p /root/.threadfin
 
 msg_info "Creating Service"
