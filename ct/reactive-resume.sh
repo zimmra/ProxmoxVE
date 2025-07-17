53fa29e01f0c7422beb2abe6a9b66e9e747ce3b3
Chris <punk.sand7393@fastmail.com>
2025-07-17 11:32:25 -0400

    Reactive Resume: switch source to community-maintained fork (#6051)
    Remove entire folder before update

--- ct/reactive-resume.sh
+++ ct/reactive-resume.sh
@@ -3,7 +3,7 @@ source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxV
 # Copyright (c) 2021-2025 community-scripts ORG
 # Author: vhsdream
 # License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
-# Source: https://rxresu.me
+# Source: https://rxresume.org
 
 APP="Reactive-Resume"
 var_tags="${var_tags:-documents}"
@@ -20,80 +20,75 @@ color
 catch_errors
 
 function update_script() {
-    header_info
-    check_container_storage
-    check_container_resources
+  header_info
+  check_container_storage
+  check_container_resources
 
-    if [[ ! -f /etc/systemd/system/Reactive-Resume.service ]]; then
-        msg_error "No ${APP} Installation Found!"
-        exit
-    fi
-    RELEASE=$(curl -fsSL https://api.github.com/repos/AmruthPillai/Reactive-Resume/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
-    if [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]] || [[ ! -f /opt/${APP}_version.txt ]]; then
-        msg_info "Stopping services"
-        systemctl stop Reactive-Resume
-        msg_ok "Stopped services"
-
-        msg_info "Updating $APP to v${RELEASE}"
-        cp /opt/${APP}/.env /opt/rxresume.env
-        res_tmp=$(mktemp)
-        rm -rf /opt/${APP}
-        curl -fsSL "https://github.com/AmruthPillai/Reactive-Resume/archive/refs/tags/v${RELEASE}.zip" -O $res_tmp
-        $STD unzip $res_tmp
-        mv ${APP}-${RELEASE}/ /opt/${APP}
-        cd /opt/${APP}
-        export PUPPETEER_SKIP_DOWNLOAD="true"
-        export NEXT_TELEMETRY_DISABLED=1
-        export CI="true"
-        export NODE_ENV="production"
-        $STD pnpm install --frozen-lockfile
-        $STD pnpm run build
-        $STD pnpm run prisma:generate
-        mv /opt/rxresume.env /opt/${APP}/.env
-        msg_ok "Updated $APP to v${RELEASE}"
-
-        msg_info "Updating Minio"
-        systemctl stop minio
-        cd /tmp
-        curl -fsSL https://dl.min.io/server/minio/release/linux-amd64/minio.deb -o minio.deb
-        $STD dpkg -i minio.deb
-        msg_ok "Updated Minio"
-
-        msg_info "Updating Browserless (Patience)"
-        systemctl stop browserless
-        cp /opt/browserless/.env /opt/browserless.env
-        rm -rf browserless
-        brwsr_tmp=$(mktemp)
-        TAG=$(curl -fsSL https://api.github.com/repos/browserless/browserless/tags?per_page=1 | grep "name" | awk '{print substr($2, 3, length($2)-4) }')
-        curl -fsSL https://github.com/browserless/browserless/archive/refs/tags/v${TAG}.zip -O $brwsr_tmp
-        $STD unzip $brwsr_tmp
-        mv browserless-${TAG}/ /opt/browserless
-        cd /opt/browserless
-        $STD npm install
-        rm -rf src/routes/{chrome,edge,firefox,webkit}
-        $STD node_modules/playwright-core/cli.js install --with-deps chromium
-        $STD npm run build
-        $STD npm run build:function
-        $STD npm prune production
-        mv /opt/browserless.env /opt/browserless/.env
-        msg_ok "Updated Browserless"
-
-        msg_info "Restarting services"
-        systemctl start minio Reactive-Resume browserless
-        msg_ok "Restarted services"
-
-        msg_info "Cleaning Up"
-        rm -f /tmp/minio.deb
-        rm -f $brwsr_tmp
-        rm -f $res_tmp
-        msg_ok "Cleanup Completed"
-
-        echo "${RELEASE}" >/opt/${APP}_version.txt
-        msg_ok "Update Successful"
-    else
-        msg_ok "No update required. ${APP} is already at v${RELEASE}"
-    fi
+  if [[ ! -f /etc/systemd/system/Reactive-Resume.service ]]; then
+    msg_error "No $APP Installation Found!"
     exit
+  fi
+  RELEASE=$(curl -fsSL https://api.github.com/repos/lazy-media/Reactive-Resume/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
+  if [[ ! -f "$HOME"/.reactive-resume ]] || [[ "$RELEASE" != "$(cat "$HOME"/.reactive-resume)" ]]; then
+    msg_info "Stopping services"
+    systemctl stop Reactive-Resume
+    msg_ok "Stopped services"
+
+    cp /opt/"$APP"/.env /opt/rxresume.env
+    rm -rf /opt/"$APP"
+    fetch_and_deploy_gh_release "Reactive-Resume" "lazy-media/Reactive-Resume"
+    msg_info "Updating $APP to v${RELEASE}"
+    cd /opt/"$APP"
+    export PUPPETEER_SKIP_DOWNLOAD="true"
+    export NEXT_TELEMETRY_DISABLED=1
+    export CI="true"
+    export NODE_ENV="production"
+    $STD pnpm install --frozen-lockfile
+    $STD pnpm run build
+    $STD pnpm run prisma:generate
+    mv /opt/rxresume.env /opt/"$APP"/.env
+    msg_ok "Updated $APP to v${RELEASE}"
+
+    msg_info "Updating Minio"
+    systemctl stop minio
+    cd /tmp
+    curl -fsSL https://dl.min.io/server/minio/release/linux-amd64/minio.deb -o minio.deb
+    $STD dpkg -i minio.deb
+    msg_ok "Updated Minio"
+
+    msg_info "Updating Browserless (Patience)"
+    systemctl stop browserless
+    cp /opt/browserless/.env /opt/browserless.env
+    rm -rf /opt/browserless
+    brwsr_tmp=$(mktemp)
+    TAG=$(curl -fsSL https://api.github.com/repos/browserless/browserless/tags?per_page=1 | grep "name" | awk '{print substr($2, 3, length($2)-4) }')
+    curl -fsSL https://github.com/browserless/browserless/archive/refs/tags/v"$TAG".zip -o "$brwsr_tmp"
+    $STD unzip "$brwsr_tmp"
+    mv browserless-"$TAG"/ /opt/browserless
+    cd /opt/browserless
+    $STD npm install
+    rm -rf src/routes/{chrome,edge,firefox,webkit}
+    $STD node_modules/playwright-core/cli.js install --with-deps chromium
+    $STD npm run build
+    $STD npm run build:function
+    $STD npm prune production
+    mv /opt/browserless.env /opt/browserless/.env
+    msg_ok "Updated Browserless"
+
+    msg_info "Restarting services"
+    systemctl start minio Reactive-Resume browserless
+    msg_ok "Restarted services"
+
+    msg_info "Cleaning Up"
+    rm -f /tmp/minio.deb
+    rm -f "$brwsr_tmp"
+    msg_ok "Cleanup Completed"
+
+    msg_ok "Update Successful"
+  else
+    msg_ok "No update required. $APP is already at v{$RELEASE}"
+  fi
+  exit
 }
 
 start
